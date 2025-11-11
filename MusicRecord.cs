bf276644013c799f41ee0633dc861ff2f43c586c
Vivor1212 <syhlerp@yahoo.dk>
2025-11-11 14:22:40 +0100

    Add project files.

---
+++ MusicRecord.cs
@@ -0,0 +1,88 @@
+﻿namespace PairProgramming
+{
+    public class MusicRecord
+    {
+        private string _title;
+        private string _artist;
+        private int _duration;
+        private int _year;
+
+        public int Id { get; set; }
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {  
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Title cannot be null.");
+                }
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Title must be more than 3 characters.");
+                }
+                _title = value;
+            }
+        }
+
+        public string Artist
+        {
+            get { return _artist; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Artist cannot be null.");
+                }
+                if (value.Length < 1)
+                {
+                    throw new ArgumentException("Artist must be more than 1 characters.");
+                }
+                _artist = value; 
+            }
+        }
+
+        public int Duration
+        {
+            get { return _duration; }
+            set
+            { 
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Duration must be a positiv number.");
+                }
+                _duration = value; 
+            }
+        }
+
+        public int Year
+        {
+            get { return _year; }
+            set
+            { 
+                if (value < 1700)
+                {
+                    throw new ArgumentException("Year must be after 1700.");
+                }
+                _year = value; 
+            }
+        }
+
+        public MusicRecord(int id, string title, string artist, int duration, int year)
+        {
+            Id = id;
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            Year = year;
+        }
+
+        public MusicRecord() : this(0, "Unknown Title", "Unknown Artist", 1, 2000) { }
+
+        public override string ToString()
+        {
+            return $"ID: {Id} - Title: {Title} - Artist: {Artist} - Duration: {Duration} - Year: {Year}";
+        }
+    }
+}
---
+++ MusicRecordRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace PairProgramming
+{
+    public class MusicRecordRepository
+    {
+        private readonly List<MusicRecord> _musicRecords = new();
+        private int _nextId = 1;
+
+        public MusicRecordRepository()
+        {
+            _musicRecords = new List<MusicRecord>()
+            {
+                new MusicRecord {Id = _nextId++, Title = "NextGen", Artist = "GenG", Duration = 500, Year = 2027},
+                new MusicRecord {Id = _nextId++, Title = "PastGen", Artist = "GG", Duration = 350, Year = 2028},
+                new MusicRecord {Id = _nextId++, Title = "FutureGen", Artist = "YesSir", Duration = 280, Year = 2029}
+            };
+        }
+
+        public IEnumerable<MusicRecord> GetAllMusicRecords()
+        {
+            IEnumerable<MusicRecord> result = new List<MusicRecord>(_musicRecords);
+
+            return result;
+        }
+    }
+}
